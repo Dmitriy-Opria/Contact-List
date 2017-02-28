69e7080b1027f1820c16efd30e81584f889b6d35
Дмитрий Опря <demoni421@gmail.com>
2017-03-01 00:21:54 +0200

    Linked with FireBase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from './services/firebase.service';
+import {Business} from "./business";
+import {Category} from "./category";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FirebaseService]
 })
-export class AppComponent {
-  constructor(af: AngularFire) {
+export class AppComponent implements OnInit{
+  businesses:Business[];
+  categories:Category[];
+  constructor(private _firebaseService: FirebaseService) {
     
+  }
+  ngOnInit(){
+    this._firebaseService.getBusinesses().subscribe(businesses => {
+      this.businesses = businesses;
+    })
+    this._firebaseService.getCategories().subscribe(categories => {
+      this.categories = categories;
+    })
   }
 }
